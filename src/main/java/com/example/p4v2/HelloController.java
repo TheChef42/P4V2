020949044c2b1e7de88b1104298f1e55a40fc435
MarcTBP <92086146+MarcTBP@users.noreply.github.com>
2023-04-18 14:23:33 +0200

    Should be able to change scene when you log in

--- src/main/java/com/example/p4v2/HelloController.java
+++ src/main/java/com/example/p4v2/HelloController.java
@@ -1,11 +1,23 @@
 package com.example.p4v2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.controlsfx.control.action.Action;
+
+import java.io.IOException;
 
 public class HelloController {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     public Label PrintName;
     @FXML
     private TextField username;
@@ -13,9 +25,8 @@ public class HelloController {
     @FXML
     private TextField password;
 
-
     @FXML
-    protected void loginButtonClick() {
+    protected void loginButtonClick(ActionEvent event) throws IOException {
         String str_username = username.getText();
         String str_password = password.getText();
         Users currentUser = Users.login(str_username,str_password);
@@ -23,9 +34,15 @@ public class HelloController {
         if (currentUser != null) {
             PrintName.setText(currentUser.getName());
             System.out.println(currentUser.getName());
+
+            //Change stage to the user start page
+            Parent root = FXMLLoader.load(getClass().getResource("startUserPage.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
         } else {
             PrintName.setText("Denied, bitch!");
         }
-
     }
 }
