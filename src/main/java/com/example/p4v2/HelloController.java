a1be691208dfbf703370ae193b0ce1da6135f61e
TheChef42 <danielflute@gmail.com>
2023-04-18 12:28:38 +0200

    Changes by marct

--- src/main/java/com/example/p4v2/HelloController.java
+++ src/main/java/com/example/p4v2/HelloController.java
@@ -2,13 +2,30 @@ package com.example.p4v2;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+
+    public Label PrintName;
     @FXML
-    private Label welcomeText;
+    private TextField username;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField password;
+
+
+    @FXML
+    protected void loginButtonClick() {
+        String str_username = username.getText();
+        String str_password = password.getText();
+        Users currentUser = Users.login(str_username,str_password);
+
+        if (currentUser != null) {
+            PrintName.setText(currentUser.getName());
+            System.out.println(currentUser.getName());
+        } else {
+            PrintName.setText("Denied, bitch!");
+        }
+
     }
 }
